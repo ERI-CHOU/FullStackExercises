983d33cd2de6f395fbccd07d3aba77d32ad52ed9
ERI-CHOU <dsayzhanglin@gmail.com>
2022-05-19 17:28:54 +0900

    complete part2.9 The Phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -7,13 +7,11 @@ const Person = ({person}) =>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456'},
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
 
   const addPersons = (event) => {
@@ -37,9 +35,17 @@ const App = () => {
 
   const handleNumberChange = (event) => setNewNumber(event.target.value)
 
+  const handleFilterChange = (event) => setNewFilter(event.target.value)
+
+  const personsToShow = newFilter.length ? persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase())) : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newFilter} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPersons}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -52,7 +58,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person => <Person key={person.name} person={person} />)}
+        {personsToShow.map(person => <Person key={person.name} person={person} />)}
     </div>
   )
 }
