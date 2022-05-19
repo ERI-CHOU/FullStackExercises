8bfa0e40e38b496f48761585d91465301d638100
ERI-CHOU <dsayzhanglin@gmail.com>
2022-05-19 14:49:49 +0900

    complete part2.4 course information step9

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -5,7 +5,7 @@ const Course = ({course}) =>
       <Total parts={course.parts} />
     </div>
 
-const Header = ({ course }) => <h1>{course}</h1>
+const Header = ({ course }) => <h2>{course}</h2>
 
 const Content = ({ parts }) => 
   <>
@@ -18,35 +18,64 @@ const Part = ({ part }) =>
   </p>
 
 const Total = ({parts}) => 
-  <p>
+  <h3>
     Number of exercises {parts.reduce((p, c) => p + c.exercises, 0)} 
-  </p>
+  </h3>
 
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const header = "Web development curriculum"
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+      <h1>{header}</h1>
+      {courses.map(course => <Course key={course.id} course={course} />)}
+    </div>
+  )
 }
 
 export default App
